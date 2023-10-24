202ab3fa345cb2916c6c319ae93cd9bb4c3b75fd
Gururaj KL <gururaj.kl@sasken.com>
2023-10-24 20:14:26 +0530

    Deployed Calculator App.

---
+++ Calculator/MVVM/CalculatorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using PropertyChanged;
+using System.Windows.Input;
+
+namespace Calculator.MVVM
+{
+    [AddINotifyPropertyChangedInterface]
+    public class CalculatorViewModel
+    {
+        public string Formula { get; set; }
+        public string Result { get; set; } = "0";
+
+        public ICommand OperationCommand => new Command((number) =>
+        {
+            Formula += number;
+        });
+
+        public ICommand ResetCommand => new Command(() =>
+        {
+            Formula = "";
+            Result = "0";
+        });
+
+        public ICommand BackspaceCommand => new Command(() =>
+        {
+            if (Formula.Length > 0)
+            {
+                Formula = Formula.Substring(0, Formula.Length - 1);
+            }
+        });
+
+        public ICommand CalculateCommand => new Command(() =>
+        {
+            if (Formula.Length == 0)
+            {
+                return;
+            }
+            Result = Dangl.Calculator.Calculator.Calculate(Formula).Result.ToString();
+        });
+    }
+}
--- Calculator/MauiProgram.cs
+++ Calculator/MauiProgram.cs
@@ -13,12 +13,10 @@ namespace Calculator
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Sublima-ExtraBold.otf", "SublimaBold");
+                    fonts.AddFont("Sublima-Light.otf", "SublimaLight");
                 });
-
-#if DEBUG
-		builder.Logging.AddDebug();
-#endif
-
+            builder.Logging.AddDebug();
             return builder.Build();
         }
     }
