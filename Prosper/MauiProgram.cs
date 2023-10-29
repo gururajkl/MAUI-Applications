ebc0b37877afc4d0712eb57d4b32ddf086120d5e
Gururaj KL <gururaj.kl@sasken.com>
2023-10-29 10:00:14 +0530

    Started to working on Prosper project.

---
+++ Prosper/MauiProgram.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Prosper
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                fonts.AddFont("Roboto-Black.ttf", "Strong");
+                fonts.AddFont("LibreFranklin-Regular.ttf", "Regular");
+            });
+
+            builder.Logging.AddDebug();
+
+            return builder.Build();
+        }
+    }
+}
