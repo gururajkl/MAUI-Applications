2f0da49aa778970e884803d39d6536cf50967da0
GururajKL <gururaj.kl@sasken.com>
2023-10-31 09:40:09 +0530

    Added clean ui views to the app.

--- DalUi/App.xaml.cs
+++ DalUi/App.xaml.cs
@@ -8,7 +8,7 @@ namespace DalUi
         {
             InitializeComponent();
 
-            MainPage = new DashboardView();
+            MainPage = new GenerationOptionView();
         }
     }
 }
--- DalUi/MauiProgram.cs
+++ DalUi/MauiProgram.cs
@@ -21,8 +21,16 @@ namespace DalUi
                     fonts.AddFont("Nexa-Heavy.ttf", "NexaHeavy");
                 });
 
+            // Removing the underline in Entry and DatePicker control.
+            Microsoft.Maui.Handlers.EditorHandler.Mapper.AppendToMapping(nameof(Editor), (handler, view) =>
+            {
+#if ANDROID
+            handler.PlatformView.SetBackgroundColor(Android.Graphics.Color.Transparent);
+#endif
+            });
+
 #if DEBUG
-		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
